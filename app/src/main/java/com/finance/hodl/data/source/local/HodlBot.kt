ce64b948003e054dd2925992c5541e2f5b83b4f5
subhransu <smohantty@gmail.com>
2024-01-28 12:36:53 +0900

    Added HodlBot entity and dao implementaion

--- app/src/main/java/com/finance/hodl/data/source/local/HodlBot.kt
+++ app/src/main/java/com/finance/hodl/data/source/local/HodlBot.kt
@@ -1,7 +1,17 @@
 package com.finance.hodl.data.source.local
 
 import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+enum class BotStatus {
+    RUNNING,
+    STOPPED
+}
 
 @Entity(tableName = "bot")
-class HodlBot {
-}
+data class HodlBot(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val status: BotStatus,
+)
--- app/src/main/java/com/finance/hodl/data/source/local/HodlBotDao.kt
+++ app/src/main/java/com/finance/hodl/data/source/local/HodlBotDao.kt
@@ -1,7 +1,25 @@
 package com.finance.hodl.data.source.local
 
 import androidx.room.Dao
+import androidx.room.Query
+import androidx.room.Upsert
+import kotlinx.coroutines.flow.Flow
 
 @Dao
-class HodlBotDao {
+interface HodlBotDao {
+
+    @Query("SELECT * FROM bot")
+    fun observeAll(): Flow<List<HodlBot>>
+
+    @Upsert
+    suspend fun upsert(bot: HodlBot)
+
+    @Upsert
+    suspend fun upsertAll(tasks: List<HodlBot>)
+
+    @Query("UPDATE bot SET status = :botStatus WHERE id = :botId")
+    suspend fun updateStatus(botId: Int, botStatus: BotStatus)
+
+    @Query("DELETE FROM bot")
+    suspend fun deleteAll()
 }
