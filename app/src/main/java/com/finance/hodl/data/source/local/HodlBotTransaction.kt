86813b721f77864568703e3eaf43dad6db3504d6
subhransu <smohantty@gmail.com>
2024-01-28 16:01:01 +0900

    linked bot table with bot_transaction table

--- app/src/main/java/com/finance/hodl/data/source/local/HodlBotTransaction.kt
+++ app/src/main/java/com/finance/hodl/data/source/local/HodlBotTransaction.kt
@@ -1,9 +1,19 @@
 package com.finance.hodl.data.source.local
 
 import androidx.room.Entity
+import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "bot_transaction")
+@Entity(tableName = "bot_transaction",
+    foreignKeys = [
+        ForeignKey(
+            entity = HodlBot::class,
+            parentColumns = ["id"],
+            childColumns = ["botId"],
+            onDelete = ForeignKey.CASCADE
+        )
+    ]
+)
 data class HodlBotTransaction(
     @PrimaryKey(autoGenerate = true)
     val id: Int = 0,
