c54021e9377044f05ac92cc686ed19c6d4df149f
subhransu <smohantty@gmail.com>
2024-02-01 20:17:05 +0900

    added extended icon dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,6 +65,8 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.2")
     implementation("androidx.navigation:navigation-compose:2.7.6")
+    implementation("androidx.compose.material:material-icons-extended:1.7.0-alpha01")
+    implementation("androidx.compose.material:material-icons-extended-android:1.7.0-alpha01")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/finance/hodl/view/icon/HodlIcons.kt
+++ app/src/main/java/com/finance/hodl/view/icon/HodlIcons.kt
@@ -1,9 +1,11 @@
 package com.finance.hodl.view.icon
 
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.CandlestickChart
 import androidx.compose.material.icons.outlined.Face
 import androidx.compose.material.icons.outlined.Home
 import androidx.compose.material.icons.outlined.Settings
+import androidx.compose.material.icons.rounded.CandlestickChart
 import androidx.compose.material.icons.rounded.Face
 import androidx.compose.material.icons.rounded.Home
 import androidx.compose.material.icons.rounded.Settings
@@ -15,8 +17,8 @@ import androidx.compose.ui.graphics.vector.ImageVector
 object HodlIcons {
     val Home = Icons.Rounded.Home
     val HomeBorder = Icons.Outlined.Home
-    val Bots = Icons.Rounded.Face
-    val BotsBorder = Icons.Outlined.Face
+    val Bots = Icons.Rounded.CandlestickChart
+    val BotsBorder = Icons.Outlined.CandlestickChart
     val Settings = Icons.Rounded.Settings
     val SettingsBorder = Icons.Outlined.Settings
 }
